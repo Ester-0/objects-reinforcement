2dda936dddd975aedde7e4a9f926f6d31ed50d4c
Ester-0 <azalea_@hotmail.com>
2023-10-30 10:40:01 +0100

    Ok

--- ex1.js
+++ ex1.js
@@ -53,15 +53,23 @@ console.log(flightReservation);
 // Observa el resultado de la consola. Escribe un console.log para cada uno de los apartados
 
 // 1. Muestra el número de vuelo
+console.log("1. " , flightReservation.flightNumber);
 
 // 2. Muestra el nombre de la aerolínea
+console.log("2. " , flightReservation.airline);
 
 // 3. Muestra TODOS los datos sobre la fecha de salida del vuelo
+console.log("3. " , flightReservation.departure);
 
 // 4. Muestra únicamente el nombre aeropuerto de llegada
+console.log("4. " , flightReservation.arrival.airport);
 
 // 5. Muestra únicamente el peso del equipaje facturado
+console.log("5. " , flightReservation.baggage.checked.totalWeight);
 
 // 6. Muestra la SUMA del peso del equipaje facturado y el equipaje de cabina
+const suma = flightReservation.baggage.checked.totalWeight + flightReservation.baggage.carryOn.totalWeight;
+console.log("6. " , suma);
 
 // 7. Usa el método includes para indicar si este vuelo dispone del servicio de Wi-Fi o no (true o false)
+console.log("7. " , flightReservation.inFlightServices.includes("Wi-Fi"));
